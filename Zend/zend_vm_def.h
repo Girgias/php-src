065e70a95e36924acb31ba3997c608e0b658c928
George Peter Banyard <girgias@php.net>
2021-11-27 14:25:16 +0000

    Implementation of function autoloading

--- Zend/zend_vm_def.h
+++ Zend/zend_vm_def.h
@@ -3692,7 +3692,10 @@ ZEND_VM_HOT_HANDLER(59, ZEND_INIT_FCALL_BY_NAME, ANY, CONST, NUM|CACHE_SLOT)
 		fbc = zend_lookup_function(Z_STR_P(function_name+1));
 		//fbc = zend_lookup_function(Z_STR_P(function_name));
 		if (UNEXPECTED(fbc == NULL)) {
-			ZEND_VM_DISPATCH_TO_HELPER(zend_undefined_function_helper);
+			if (EXPECTED(!EG(exception))) {
+				ZEND_VM_DISPATCH_TO_HELPER(zend_undefined_function_helper);
+			}
+			HANDLE_EXCEPTION();
 		}
 		if (EXPECTED(fbc->type == ZEND_USER_FUNCTION) && UNEXPECTED(!RUN_TIME_CACHE(&fbc->op_array))) {
 			init_func_run_time_cache(&fbc->op_array);
@@ -3833,10 +3836,10 @@ ZEND_VM_HOT_HANDLER(69, ZEND_INIT_NS_FCALL_BY_NAME, ANY, CONST, NUM|CACHE_SLOT)
 		zval *function_name = (zval *)RT_CONSTANT(opline, opline->op2);
 		fbc = zend_lookup_function(Z_STR_P(function_name+1));
 		if (fbc == NULL) {
-			fbc = zend_lookup_function(Z_STR_P(function_name+2));
-			if (fbc == NULL) {
+			if (EXPECTED(!EG(exception))) {
 				ZEND_VM_DISPATCH_TO_HELPER(zend_undefined_function_helper);
 			}
+			HANDLE_EXCEPTION();
 		}
 		if (EXPECTED(fbc->type == ZEND_USER_FUNCTION) && UNEXPECTED(!RUN_TIME_CACHE(&fbc->op_array))) {
 			init_func_run_time_cache(&fbc->op_array);
@@ -3863,7 +3866,10 @@ ZEND_VM_HOT_HANDLER(61, ZEND_INIT_FCALL, NUM, CONST, NUM|CACHE_SLOT)
 		zval *function_name = (zval *)RT_CONSTANT(opline, opline->op2);
 		fbc = zend_lookup_function(Z_STR_P(function_name));
 		if (UNEXPECTED(fbc == NULL)) {
-			ZEND_VM_DISPATCH_TO_HELPER(zend_undefined_function_helper);
+			if (EXPECTED(!EG(exception))) {
+				ZEND_VM_DISPATCH_TO_HELPER(zend_undefined_function_helper);
+			}
+			HANDLE_EXCEPTION();
 		}
 		if (EXPECTED(fbc->type == ZEND_USER_FUNCTION) && UNEXPECTED(!RUN_TIME_CACHE(&fbc->op_array))) {
 			init_func_run_time_cache(&fbc->op_array);
